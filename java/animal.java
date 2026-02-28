a5484f78e930e4444e804df32de570c4e16e929c
Princess James <112994424+princessasinjames@users.noreply.github.com>
2026-02-28 02:15:08 -0500

    Add files via upload

---
+++ java/animal.java
@@ -0,0 +1,5 @@
+public class animal {
+    public static String iAmCat() {
+        return "I am a cat";
+    }
+}
---
+++ java/numbers.java
@@ -0,0 +1,43 @@
+
+public class numbers {
+    public static void main(String[] args){
+        /*
+        // Variables
+        String fullName = "Jack Doe";
+        int quantity = 20;
+        boolean isStudent = false;
+
+        // Print statements (sout + Tab is faster)
+        System.out.println("\nYour name is " + fullName);
+        System.out.println("There are " + quantity + " boxes");
+        if (isStudent) {
+            System.out.println("You are a student");
+        } else{
+            System.out.println("You are not a student");
+        }
+        */
+
+        /* Input:
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        scanner.nextLine();
+
+        scanner.close();
+        */
+
+        /* Test iAmCat function
+        String exclaim = addExclamationPoint("Pasta");
+        System.out.println(exclaim);
+
+        animal a = new animal();
+        String cat = a.iAmCat();
+        System.out.println(cat);
+        */
+    }
+
+    // Function to add an exclamation
+    public static String addExclamationPoint(String s) {
+        return s + "!";
+    }
+}
